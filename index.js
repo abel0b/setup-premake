8c96e76d62db4c5cbe7c16d7e5b80183534374c7
Abel Calluaud <abel0b@protonmail.com>
2020-11-21 21:09:03 +0100

    Fix github action

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const tc = require("@actions/tool-cache")
 async function main() {
     if (process.platform == "win32") {
         const premake = await tc.downloadTool("https://github.com/premake/premake-core/releases/download/v5.0.0-alpha15/premake-5.0.0-alpha15-windows.zip")
-        await premake.extractZip(node12Path, '.premake')
+        await tc.extractZip(premake, '.premake')
     }
     else if (process.platform == "darwin") {
         const premake = await tc.downloadTool("https://github.com/premake/premake-core/releases/download/v5.0.0-alpha15/premake-5.0.0-alpha15-macosx.tar.gz")
-        await premake.extractTar(node12Path, '.premake')
+        await tc.extractTar(premake, '.premake')
     }
     else {
         const premake = await tc.downloadTool("https://github.com/premake/premake-core/releases/download/v5.0.0-alpha15/premake-5.0.0-alpha15-linux.tar.gz")
-        await premake.extractTar(node12Path, '.premake')
+        await tc.extractTar(premake, '.premake')
     }
     core.addPath(".premake")
 }
