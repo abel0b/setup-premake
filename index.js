bc2a1d8abcee0499e219e310dcc12c5b6dfc6d0a
checkraisefold <devsmash1@gmail.com>
2021-08-21 21:12:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ async function main() {
     const version = core.getInput('version', { required: true })
     const common = path + "v" + version + "/premake-" + version
     if (process.platform == "win32") {
-        const premake = await tc.downloadTool(common + "-windows.zip")
         core.setFailed("win32" + common)
+        const premake = await tc.downloadTool(common + "-windows.zip")
         await tc.extractZip(premake, '.premake')
     }
     else if (process.platform == "darwin") {
-        const premake = await tc.downloadTool(common + "-macosx.tar.gz")
         core.setFailed("darwin" + common)
+        const premake = await tc.downloadTool(common + "-macosx.tar.gz")
         await tc.extractTar(premake, '.premake')
     }
     else {
-        const premake = await tc.downloadTool(common + "-linux.tar.gz")
         core.setFailed("linux" + common)
+        const premake = await tc.downloadTool(common + "-linux.tar.gz")
         await tc.extractTar(premake, '.premake')
     }
     core.addPath(".premake")
